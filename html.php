51e580b7aef429eea73280954ec87e894b4bf192
johnlimoran110491 <52062740+johnlimoran110491@users.noreply.github.com>
2019-06-21 13:59:06 +0800

    Add files via upload

---
+++ html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>EasyAsCash</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+		<meta charset="utf-8">
+  <meta name='viewport'  content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
+
+<!-- Website CSS style -->
+
+<link rel='shortcut icon' href='assets/images/logo.jpg' type='image/jpeg' />
+<!-- Website Font style -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+<!-- Google Fonts -->
+<link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+<link href='assets/css/web.css' rel='stylesheet' type='text/css'>
+</head>
---
+++ js.php
@@ -0,0 +1,4 @@
+<script type='text/javascript' src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type='text/javascript' src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<script type='text/javascript' src='assets/js/web.js'></script>
+</html>
